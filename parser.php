3cfea1b4c349532e5af358b244294cf903908258
tshevchenko271285 <tshevchenko@ro.ru>
2017-09-10 20:16:10 +0300

    last

--- parser.php
+++ parser.php
@@ -15,26 +15,32 @@ $res = json_decode($file);
 $res = $res->response;
 
 //	Готовим массив с данными для отображения, на основании ответа от сервера
-
 $data = [];
-
+$data_not_name = [];
 for($i=0; $i<count($res); $i++) {
+
+	//Если нет названия оружия сохраняем в отдельный массив.
+	if(empty($res[$i]->d)) {
+		$data_not_name[$i]['name'] = $res[$i]->d."Нет имени. Оружие типа: ".$res[$i]->t;
+		$data_not_name[$i]['cena'] = (float) trim(strip_tags($res[$i]->v));
+		$data_not_name[$i]['cnt'] = 1;
+		$data_not_name[$i]['date'] = date('Y-n-d G:i:s');
+		continue;
+	}
 	
+	// Если название оружия число(индекс), тогда увеличиваем 
+	// колличество этого оружия обращаясь по индексу.
 	if(is_int($res[$i]->d)){
 		$data[$res[$i]->d]['cnt']++;
 		continue;
 	}
+	
 	//Сохраняем необходимые данные
 	$data[$i]['name'] = trim(strip_tags($res[$i]->d));
 	$data[$i]['cena'] = (float) trim(strip_tags($res[$i]->v));
 	$data[$i]['cnt'] = 1;
 	$data[$i]['date'] = date('Y-n-d G:i:s');
-	
-	if(empty($data[$i]['name'])) {
-		$data[$i]['name'] = "Нет имени. Оружие типа: ".$res[$i]->t;
-	}
-
 }
+
 header('Content-type: application/json; charset=utf8');
 echo json_encode($data);
-
